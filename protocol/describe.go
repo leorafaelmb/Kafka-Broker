568311ea55170308f8fdca3a2b3d7d4d1ce62ecc
leorafaelmb <leorafaelmb@gmail.com>
2025-11-19 12:27:40 -0500

    Add structs to represent DescribeTopicPartitions messages, add describe.go

--- protocol/describe.go
+++ protocol/describe.go
@@ -1,1 +1,43 @@
 package protocol
+
+type DescribeTPRequest struct {
+	Topics                 []string // compact array => compact string
+	ResponsePartitionLimit int32
+	Cursor
+}
+
+type Cursor struct {
+	TopicName      string // compact string
+	PartitionIndex int32
+}
+
+type DescribeTPResponse struct {
+	ThrottleTime int32
+}
+
+type Topic struct {
+	ErrorCode                 int16
+	Contents                  string // compact string
+	TopicID                   int64
+	IsInternal                bool
+	Partitions                []Partition
+	TopicAuthorizedOperations int32
+	NextCursor                Cursor
+}
+
+type Partition struct {
+	ErrorCode              int16
+	PartitionIndex         int32
+	LeaderID               int32
+	LeaderEpoch            int32
+	ReplicaNodes           int32
+	ISRNodes               int32
+	EligibleLeaderReplicas int32
+	LastKnownELR           int32
+	OfflineReplicas        int32
+}
+
+func Deserialize(rbr ReaderByteReader) DescribeTPRequest {
+
+	return DescribeTPRequest{}
+}
